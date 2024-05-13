f37a125654a5e2d231e8ba2c55ac51f698b982ee
fiadaze <fiakantapon@gmail.com>
2024-05-13 14:22:58 +0700

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+const products = {
+    products_id: '1',
+    product_name: "Macbook Air",
+    quantity_in_stock: 999,
+    unit_price:1599,
+    product_description :{
+        product_brand : "Apple",
+        size_or_quantity : 999999,
+        taste_or_scent : "Aluminum",
+        colour : ["Black","Blue","Pink","Green","Gray"],
+        additional_description : "Trust me android is better."
+    },
+};
